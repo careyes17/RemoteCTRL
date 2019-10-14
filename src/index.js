2ed1bfe9679b513f0167a59d15eb5d2ce886a6c0
Carlos <careyes4820@eagle.fgcu.edu>
2019-10-14 16:08:01 -0400

    Developing Homepage Card

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Crane from './views/Crane/Crane';
 import PrinterInterface from './views/PrinterInterface/PrinterInterface';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Card from '../../components/Home/Card/card'
+import './Home.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='center'>
+      <Card test='•'/>
+    </div>
   );
 }
 
