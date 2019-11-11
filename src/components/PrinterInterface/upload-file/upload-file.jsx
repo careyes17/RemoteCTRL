a666970792ee7d0c732cb6f2876fe9a3ff40c062
Carlos <careyes4820@eagle.fgcu.edu>
2019-11-11 16:38:02 -0500

    Input file functioning, needs state storage

--- src/components/PrinterInterface/upload-file/upload-file.jsx
+++ src/components/PrinterInterface/upload-file/upload-file.jsx
@@ -1,12 +1,53 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import AttachFileIcon from '@material-ui/icons/AttachFile';
-import './upload-file.css'
+import React from "react";
+import Button from "@material-ui/core/Button";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import "./upload-file.css";
 
-  export default function UploadFile({gridElement}) {
+class UploadFile extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      file: '',
+      safeToPrint: false,
+    };
+  }
+
+  parsegcode() {
+    var gcodeFileObject = this.refs.gcode.files[0];
+    if (gcodeFileObject === undefined) {
+      this.setState({
+        file: '',
+        safeToPrint: false,
+      });
+      return;
+    };
+    console.log(gcodeFileObject);
+    this.setState({
+      file: gcodeFileObject.name,
+      safeToPrint: true,
+    });
+  }
+
+  render() {
     return (
-      <Button variant="outlined" className={`${gridElement} button-upload-file`}>
-        Upload File &nbsp; <AttachFileIcon/>
+      <Button
+        variant="outlined"
+        component="label"
+        className={`${this.props.gridElement} button-upload-file`}
+      >
+        Upload File &nbsp; <AttachFileIcon />
+        <input
+          id="gcode-input"
+          type="file"
+          style={{ display: "none" }}
+          ref="gcode"
+          onChange={this.parsegcode.bind(this)}
+        />
+        {this.state.file}
       </Button>
     );
   }
+}
+
+export default UploadFile;
